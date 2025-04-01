224455404f53144bf352bf1e6326ad07d0df7453
Aya7m <yyoyo6987@gmail.com>
2025-04-01 14:29:03 +0200

    addRegister

--- routers/auth.route.js
+++ routers/auth.route.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import { protect } from "../middleware/protectetRoute.js";
-import { getProfile, registerUser, sendOTP, verifyOTP } from "../controllers/auth.controller.js";
+import { getProfile,  registerUserAndSendOTP,  verifyOTP } from "../controllers/auth.controller.js";
 
 const authRoute=Router()
 
-authRoute.post('/register', registerUser); // راوت جديد لتسجيل المستخدم
+// authRoute.post('/register', registerUser); // راوت جديد لتسجيل المستخدم
 
 // authRoute.post('/send-otp',sendOTP)
+authRoute.post('/send',registerUserAndSendOTP)
 authRoute.post('/verify-otp',verifyOTP)
 authRoute.get('/profile',protect,getProfile)
 
