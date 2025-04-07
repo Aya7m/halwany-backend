45a8ddab54d5e869194a4985355ea34938e8506c
Aya7m <yyoyo6987@gmail.com>
2025-04-07 12:04:08 +0200

    editcors

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ const port = process.env.PORT || 5000
 
 
 
-app.use(express.json({ limit: "50mb" }))
+
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://halwany-backend.vercel.app'],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    origin: "http://localhost:3000", // أو دومين موقعك بعد النشر
+    credentials: true // لو بتستخدمي الكوكيز أو الهيدر فيه بيانات حساسة
 }));
 
+app.use(express.json({ limit: "50mb" }))
 
 app.use('/auth', authRoute)
 app.use('/product', productRoute)
 app.use('/cart', cartRouter)
-app.use('/payment',paymentRoute)
-app.use('/review',reviewRouter)
-app.use('/coupen',coupenRouter)
+app.use('/payment', paymentRoute)
+app.use('/review', reviewRouter)
+app.use('/coupen', coupenRouter)
 connectDB()
 
 
