afbb1f87e67c13812bb6f98cdffd3046e3c1d3e7
Aya7m <yyoyo6987@gmail.com>
2025-04-07 13:54:49 +0200

    edit

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -2,9 +2,9 @@ import cloudinary from "../lib/cloudinary.js";
 import { Product } from "../models/product.model.js";
 
 export const createProduct = async (req, res) => {
-    try {
-        const {name, description, price,image} = req.body
-        let cloudinaryResponse = null;
+	try {
+		const { name, description, price, image } = req.body
+		let cloudinaryResponse = null;
 
 		if (image) {
 			cloudinaryResponse = await cloudinary.uploader.upload(image, { folder: "products" });
@@ -19,51 +19,51 @@ export const createProduct = async (req, res) => {
 		});
 
 		res.status(201).json(product);
-        
-    } catch (error) {
-        console.log("Error in createProduct controller", error.message);
+
+	} catch (error) {
+		console.log("Error in createProduct controller", error);
 		res.status(500).json({ message: "Server error", error: error.message });
-        
-    }
+
+	}
 }
 
 export const getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("Error in getAllProducts controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
-} 
+	try {
+		const products = await Product.find({});
+		res.status(200).json(products);
+	} catch (error) {
+		console.log("Error in getAllProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
+}
 
 // get featured products
 export const getFeaturedProducts = async (req, res) => {
-    try {
-        const products = await Product.find({ isFeatured: true });
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("Error in getFeaturedProducts controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+	try {
+		const products = await Product.find({ isFeatured: true });
+		res.status(200).json(products);
+	} catch (error) {
+		console.log("Error in getFeaturedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 // get product by category
 export const getProductsByCategory = async (req, res) => {
-    try {
-        const { category } = req.params;
-        const products = await Product.find({ category });
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("Error in getProductsByCategory controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+	try {
+		const { category } = req.params;
+		const products = await Product.find({ category });
+		res.status(200).json(products);
+	} catch (error) {
+		console.log("Error in getProductsByCategory controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 // get recommended products
 export const getRecommendedProducts = async (req, res) => {
-    try {
-        const products = await Product.aggregate([
+	try {
+		const products = await Product.aggregate([
 			{
 				$sample: { size: 4 },
 			},
@@ -79,40 +79,40 @@ export const getRecommendedProducts = async (req, res) => {
 		]);
 
 		res.json(products);
-    } catch (error) {
-        console.log("Error in getRecommendedProducts controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+	} catch (error) {
+		console.log("Error in getRecommendedProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 // get best selling products
 export const getBestSellingProducts = async (req, res) => {
-    try {
+	try {
 
-        const products = await Product.find({ isBestSelling: true });
-        res.status(200).json(products);
-    } catch (error) {
-        console.log("Error in getBestSellingProducts controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+		const products = await Product.find({ isBestSelling: true });
+		res.status(200).json(products);
+	} catch (error) {
+		console.log("Error in getBestSellingProducts controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 // get product by name
 export const getProductByName = async (req, res) => {
-    try {
-        const { name } = req.params;
-        const product = await Product.findOne({ name });
-        res.status(200).json(product);
-    } catch (error) {
-        console.log("Error in getProductByName controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+	try {
+		const { name } = req.params;
+		const product = await Product.findOne({ name });
+		res.status(200).json(product);
+	} catch (error) {
+		console.log("Error in getProductByName controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 // delete product by id
 export const deleteProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
+	try {
+		const product = await Product.findById(req.params.id);
 
 		if (!product) {
 			return res.status(404).json({ message: "Product not found" });
@@ -131,10 +131,10 @@ export const deleteProductById = async (req, res) => {
 		await Product.findByIdAndDelete(req.params.id);
 
 		res.json({ message: "Product deleted successfully" });
-    } catch (error) {
-        console.log("Error in deleteProductById controller", error.message);
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
+	} catch (error) {
+		console.log("Error in deleteProductById controller", error.message);
+		res.status(500).json({ message: "Server error", error: error.message });
+	}
 }
 
 export const toggleFeaturedProduct = async (req, res) => {
@@ -160,7 +160,7 @@ async function updateFeaturedProductsCache() {
 
 		const featuredProducts = await Product.find({ isFeatured: true }).lean();
 
-	
+
 	} catch (error) {
 		console.log("error in update cache function");
 	}
