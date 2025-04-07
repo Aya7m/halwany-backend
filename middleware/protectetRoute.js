687e51eeb209738ae23d1e2d709e7865b282fe9d
Aya7m <yyoyo6987@gmail.com>
2025-04-07 16:25:15 +0200

    ediRole

--- middleware/protectetRoute.js
+++ middleware/protectetRoute.js
@@ -3,29 +3,32 @@ import { User } from "../models/user.model.js";
 
 export const protect = async (req, res, next) => {
   try {
-		const { token } = req.headers;
-        console.log("🔑 Token received:", token);
+    const { token } = req.headers;
+    console.log("🔑 Token received:", token);
 
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized - No token" });
-        }
-
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log("📜 Decoded token:", decoded);
-
-        const user = await User.findById(decoded.userId).select("-password");
-        console.log("👤 User found:", user);
-
-        if (!user) {
-            return res.status(401).json({ message: "Unauthorized - User not found" });
-        }
-
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized - No token" });
     }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("📜 Decoded token:", decoded);
+
+    const user = await User.findById(decoded.id).select("-password");
+    console.log("👤 User found:", user);
+
+    if (!user) {
+      return res.status(401).json({ message: "Unauthorized - User not found" });
+    }
+
+    req.user = user;
+    next();
+
+  } catch (error) {
+    console.error("❌ Token error:", error.message);
+    return res.status(401).json({ message: "Unauthorized - Invalid token" });
+  }
 };
+
 
 
 export const adminProtect = (req, res, next) => {
