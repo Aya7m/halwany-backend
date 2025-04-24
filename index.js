dd36bf30ede833715a363b07a5522b00321d5114
Aya7m <yyoyo6987@gmail.com>
2025-04-24 19:48:21 +0200

    editCors

--- index.js
+++ index.js
@@ -21,10 +21,30 @@ const port = process.env.PORT || 5000
 
 
 
-app.use(cors({
-    origin: "http://localhost:3000", // أو دومين موقعك بعد النشر
-    credentials: true // لو بتستخدمي الكوكيز أو الهيدر فيه بيانات حساسة
-}));
+// app.use(cors({
+//     origin: "http://localhost:3000", // أو دومين موقعك بعد النشر
+//     credentials: true // لو بتستخدمي الكوكيز أو الهيدر فيه بيانات حساسة
+// }));
+
+
+
+const allowedOrigins = [
+    'http://localhost:3000',
+    'http://192.168.56.1:3000',
+    'https://your-prod-domain.com'
+  ];
+  
+  app.use(cors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true
+  }));
+  
 
 app.use(express.json({ limit: "50mb" }))
 
